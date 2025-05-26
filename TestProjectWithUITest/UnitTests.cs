96cfdae5c42bad3e1c38cee149010fa2d4af7119
ruspyaloneworker <DioBrando2003@yandex.ru>
2025-05-26 22:25:12 +0300

    20 синхронно

--- TestProjectWithUITest/UnitTests.cs
+++ TestProjectWithUITest/UnitTests.cs
@@ -1,12 +1,18 @@
 namespace TestProjectWithUITest;
 
 [TestFixture]
-[Parallelizable(ParallelScope.All)]
+// [Parallelizable(ParallelScope.All)]
 public class UnitTests: BaseTest
 {
-	[TestCase(typeof(ClickCalculatorLink))]
-	[TestCase(typeof(Calculator2Plus2Result))]
-	[TestCase(typeof(Calculator2Minus2Result))]
+	private static IEnumerable<Type> GetTestScenarios()
+	{
+		for (int i = 0; i < 20; i++)
+		{
+			yield return typeof(Calculator2Minus2Result);
+		}
+	}
+
+	[TestCaseSource(nameof(GetTestScenarios))]
 	public async Task GithubTests(Type testScenario)
 	{
 		await _uiTest.RunTestAsync(testScenario);
